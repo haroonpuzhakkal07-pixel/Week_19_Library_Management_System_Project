4d07794b53d406cd4632db32b8d7dc4fb84edbf4
haroonpuzhakkal07-pixel <haroonpuzhakkal07@gmail.com>
2026-04-02 15:51:08 +0530

    Added Book and BorrowRecord models with admin setup

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
