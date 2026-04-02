6c61e2de30078b1e24e81e80257b24cfe83e44c1
haroonpuzhakkal07-pixel <haroonpuzhakkal07@gmail.com>
2026-04-02 18:01:35 +0530

    Implemented borrow and return book functionality with availability logic

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BorrowRecordViewSet, register
+from .views import BookViewSet, BorrowRecordViewSet, register, borrow_book, return_book
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -10,5 +10,7 @@ router.register(r'borrow-records', BorrowRecordViewSet)
 urlpatterns = router.urls + [
     path('register/', register),
     path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='refresh')
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('borrow/<int:book_id>/', borrow_book),
+    path('return/<int:book_id>/', return_book),
 ]
--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -24,4 +25,49 @@ def register(request):
         serializer.save()
         return Response({'message': 'User Created Successfully.'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def borrow_book(request, book_id):
+    user = request.user
+    book = get_object_or_404(Book, id=book_id)
+
+    # Check Availability
+    if not book.available:
+        return Response(
+            {'error': 'Book is not available'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    # Create Borrow Record
+    BorrowRecord.objects.create(user=user, book=book)
+    # Mark Book Unavailable
+    book.available = False
+    book.save()
+    return Response({'Message': 'Book borrowed succesfully'})
     
+@api_view(['POST'])
+def return_book(request, book_id):
+    user = request.user
+    book = get_object_or_404(Book, id=book_id)
+    # Find Active Borrow Record
+    record = BorrowRecord.objects.filter(
+        user=user,
+        book=book,
+        returned=False
+    ).first()
+    
+    if not record:
+        return Response(
+            {'error': 'No active borrow record found'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    # Mark as returned
+    record.returned = True
+    record.save()
+
+    # Make book available again
+    book.available = True
+    book.save()
+
+    return Response(
+        {'message': 'Book returned successfully'}
+    )
